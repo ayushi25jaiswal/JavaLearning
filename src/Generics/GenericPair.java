f5b468d6b8e3107827e52462834b115adaf5b1a6
ayushi jaiswal <ayushijaiswal2507@gmail.com>
2026-05-26 19:46:41 +0530

    GenericsPair added

--- src/Generics/GenericPair.java
+++ src/Generics/GenericPair.java
@@ -1,4 +1,25 @@
 package Generics;
 
-public class GenericPair {
+public class GenericPair <T, V>{
+    private T first;
+    private V second;
+
+
+    public V getSecond() {
+        return second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+
 }
--- src/Generics/Main.java
+++ src/Generics/Main.java
@@ -5,12 +5,24 @@ public class Main {
     static void main(String[] args) {
 
         Pair p = new Pair();
-
+  //------------------------------------------------------------------------------------------
         //let first be name-> String, second be psp-> double
         p.setFirst("Ayushi");
         p.setSecond(9.0);
 
         p.setFirst(10);//it is not giving error as you cant be strict on the type using Object.
+
+ // ------------------------------------------------------------------------------------------
+
+        GenericPair<String, Double> student = new GenericPair<>();
+
+        student.setFirst("Ayushi");
+        student.setSecond(99.0);
+
+//        student.setFirst(90);//this will throw error at compile time only.
+        //for every object you can create object with different type. But for that particular object that is fixed.
+        //use for stack - storing Integer, character, string etc.
+
 
     }
 }
